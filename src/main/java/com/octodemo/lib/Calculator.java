21c9a77e4cf25c8e77cc0a4e7e891088d7bc04fb
Tugdual Grall <tgrall@github.com>
2022-02-05 13:08:25 +0100

    new version

--- src/main/java/com/octodemo/lib/Calculator.java
+++ src/main/java/com/octodemo/lib/Calculator.java
@@ -1,34 +1,15 @@
 package com.octodemo.lib;
 
-/**
- * Hello world!
- * 
- */
 public class Calculator
 {
 
-    public String greetings = "Welcome to the Calculator API";
-
-
     public int add(int a, int b) {
-        System.out.println( greetings.toUpperCase() );
         return a + b;
     }
 
 
     public int sub(int a, int b) {
         return a - b;
-    }
-
-    public String toUpper(String s) {
-        if (s.equals("")) {
-            return null;
-        }
-        return s.toUpperCase();
-    }
-
-    private boolean isThataGoodMethod(int a, int b) {
-        return false;
     }
 
 }
